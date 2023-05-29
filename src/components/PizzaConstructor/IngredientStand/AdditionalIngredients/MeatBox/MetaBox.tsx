793fbb4101891a99c3202043105325be61aaf060
Kulesh Uladzislau <kulesh_uadzislau@mail.ru>
2023-05-30 00:17:59 +0300

    first commit

---
+++ src/components/PizzaConstructor/IngredientStand/AdditionalIngredients/MeatBox/MetaBox.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const MeatBox = () => {
+    return (
+        <div>
+            <h5>Мясо:</h5>
+            <div>
+                <input type="checkbox"/><span>курица</span>
+                <input type="checkbox"/><span>бекон</span>
+                <input type="checkbox"/><span>салями</span>
+                <input type="checkbox"/><span>пеперони</span>
+            </div>
+        </div>
+    )
+
+}
